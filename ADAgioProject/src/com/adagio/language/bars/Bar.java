9bd37660d2dc30c5b80d048979aa88d795374c76
juanhg <juanhgw@gmail.com>
2013-12-12 17:55:11 +0100

    Minor changes

--- ADAgioProject/src/com/adagio/language/bars/Bar.java
+++ ADAgioProject/src/com/adagio/language/bars/Bar.java
@@ -1,11 +1,8 @@
 package com.adagio.language.bars;
 
-import org.modelcc.Associativity;
-import org.modelcc.AssociativityType;
 import org.modelcc.IModel;
 import org.modelcc.Multiplicity;
 
-@Associativity(AssociativityType.RIGHT_TO_LEFT)
 public class Bar implements IModel {
 	@Multiplicity(minimum = 1)
 	BarItem [] barItems;
--- ADAgioProject/src/com/adagio/language/statements/TempoStatement.java
+++ ADAgioProject/src/com/adagio/language/statements/TempoStatement.java
@@ -5,7 +5,7 @@ import org.modelcc.Prefix;
 import org.modelcc.Priority;
 
 import com.adagio.events.MusicEventListener;
-@Priority(value=1)
+
 @Prefix("(?i)TEMPO")
 public abstract class TempoStatement extends Statement implements IModel {
 	
