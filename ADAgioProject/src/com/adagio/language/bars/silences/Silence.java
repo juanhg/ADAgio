be5ec6d613726108cb40bf364f2078d94a3bb77b
juanhg <juanhgw@gmail.com>
2013-12-14 14:25:40 +0100

    Minor Changes in Silences.

--- ADAgioProject/src/com/adagio/language/bars/silences/Silence.java
+++ ADAgioProject/src/com/adagio/language/bars/silences/Silence.java
@@ -6,7 +6,7 @@ import org.modelcc.Value;
 
 import com.adagio.language.bars.BarItem;
 
-@Pattern(regExp = "_|S|s")
+@Pattern(regExp = "_|S|s|r|R")
 public class Silence extends BarItem implements IModel {
 	@Value
 	String value;
